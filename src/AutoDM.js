8e84fa9106c1c6cb24c27fc38bfda333eabd778f
Dobrogostov <39377437+Dobrogostov@users.noreply.github.com>
2018-05-24 12:50:42 +0200

    Update AutoDM.js

--- src/AutoDM.js
+++ src/AutoDM.js
@@ -41,7 +41,7 @@ const GenerateMessage = name => {
   ];
   const d = new Date();
   const dayName = days[d.getDay()];
-  return `Hi ${name} Thanks for .... \n Happy ${dayName} 😊😊 `; // your message
+  return `Salut ${name}. Bienvenue sur France Airdrop ! Retrouve une sélection des meilleurs du jour sur T.me/france_airdrop !😇 `; // your message
   // My message   return `Hi ${name} Thanks for being a part of my social media network. I'am the @PicsrushE founder,A new Online Image Editor completely with web technologies,I'm also a reactjs developer and medium blogger.\n Happy to discuss anytime 😊  \n Happy ${dayName} 😊😊 `;
 };
 
